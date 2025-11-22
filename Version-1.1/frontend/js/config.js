37275e10a43f385790842fcee1df7d88ddb6aacf
Rosinaer <rosinaer@hotmail.com>
2025-11-22 00:26:54 -0300

    Actualizo URL del backend a Render

--- Version-1.1/frontend/js/config.js
+++ Version-1.1/frontend/js/config.js
@@ -1,6 +1,1 @@
-const isLocalhost = window.location.hostname === 'localhost';
-
-const BACKEND_URL = isLocalhost
-  ? 'http://localhost:3000'
-  : 'https://los-voraces-app-backend.onrender.com';
-
+const BACKEND_URL = "https://losvoracesapp-devops.onrender.com";
