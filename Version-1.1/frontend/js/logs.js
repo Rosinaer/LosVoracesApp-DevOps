b15a30c2ddb3653e0b2d7f60e5c9702b8b41b982
Agustina Bartolomé <agustina.bartolome11@gmail.com>
2025-11-24 11:35:09 -0300

    centralización de logevent

--- Version-1.1/frontend/js/logs.js
+++ Version-1.1/frontend/js/logs.js
@@ -1,3 +1,5 @@
+const isProduction = location.hostname !== "localhost";
+
 async function logEvent(event, details = {}) {
   try {
     await fetch(`${BACKEND_URL}/logs`, {
@@ -6,6 +8,13 @@ async function logEvent(event, details = {}) {
       body: JSON.stringify({ event, details })
     });
   } catch (err) {
-    console.error("No se pudo enviar log:", err);
+    //console.error("No se pudo enviar log:", err);
+    debugError(err, `Error enviando log: ${event}`);
+  }
+}
+
+function debugError(err, context = "") {
+  if (!isProduction) {
+    console.error(context, err);
   }
 }
